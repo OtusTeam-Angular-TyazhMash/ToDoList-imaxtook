70af09505a8dcde3c94d199f2300bbe106477ae7
91342 <91342@tyazhmash.com>
2024-08-16 11:18:18 +0400

    homework4

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { AppComponent } from './app.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+import { ListItemComponent } from './components/to-do-list/list-item/list-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToDoListComponent,
+    ListItemComponent
+  ],
+  imports: [
+    BrowserModule, 
+    FormsModule, 
+    BrowserAnimationsModule, 
+    MatInputModule,
+    MatProgressSpinnerModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
