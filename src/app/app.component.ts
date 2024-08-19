99575eda40f2c6fadcac3d29cf7afa7d3ca3c849
91342 <91342@tyazhmash.com>
2024-08-19 16:27:38 +0400

    Homework10

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, } from '@angular/core';
+import { DataService } from './services/data.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'TasksBoard';
+
+  constructor(
+    protected data: DataService,
+    public router: Router
+  ) {}
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+
+import { AppComponent } from './app.component';
+import { BacklogComponent } from './components/backlog/backlog.component';
+import { ListItemComponent } from './components/backlog/list-item/list-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { TodoCreateItemComponent } from './components/backlog/todo-create-item/todo-create-item.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutesComponent } from './app.routes';
+import { ToDoItemViewComponent } from './components/backlog/to-do-item-view/to-do-item-view.component';
+import { BoardComponent } from './components/board/board.component';
+import { MatMenuModule } from '@angular/material/menu'
+import { MatIconModule } from '@angular/material/icon'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BacklogComponent,
+    ListItemComponent,
+    TodoCreateItemComponent,
+    ToDoItemViewComponent,
+    BoardComponent
+  ],
+  imports: [
+    BrowserModule, 
+    FormsModule, 
+    HttpClientModule,
+    BrowserAnimationsModule, 
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    SharedModule,
+    AppRoutesComponent,
+    MatMenuModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
