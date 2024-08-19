9dab28d1a0c650e6938d06815e1efa65828f344d
91342 <91342@tyazhmash.com>
2024-08-19 16:14:42 +0400

    Homework8

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+
+import { AppComponent } from './app.component';
+import { ToDoListComponent } from './components/backlog/backlog.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { TodoCreateItemComponent } from './components/todo-create-item/todo-create-item.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutesComponent } from './app.routes';
+import { ToDoItemViewComponent } from './components/to-do-item-view/to-do-item-view.component';
+import { BoardComponent } from './components/board/board.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToDoListComponent,
+    ListItemComponent,
+    TodoCreateItemComponent,
+    ToDoItemViewComponent,
+    BoardComponent
+  ],
+  imports: [
+    BrowserModule, 
+    FormsModule, 
+    HttpClientModule,
+    BrowserAnimationsModule, 
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    SharedModule,
+    AppRoutesComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
